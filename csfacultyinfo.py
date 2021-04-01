d5196d6821d0ba1854f8ce080feb5a0aecc58ee3
Travis A. Dula <travisadula@gmail.com>
2021-04-01 00:27:10 -0500

    scraper finished

---
+++ csfacultyinfo.py
@@ -0,0 +1,26 @@
+import requests
+import json
+from bs4 import BeautifulSoup
+
+url = 'https://cs.utdallas.edu/people/faculty/'
+
+def main():
+    req = requests.get(url)
+    soup = BeautifulSoup(req.text, features='html5lib').tbody
+    profs_html = soup.find_all('tr')
+    profs_dict = { tr.td.find_all('a')[-1].text : {
+        'utd_site': tr.td.find_all('a')[-1].get('href'),
+        'position': ' '.join(tr.td.text.split('\n')[2:]).strip(),
+        'email': tr.find_all('td')[1].a.text,
+        'phone': tr.find_all('td')[1].text.strip().split('\n')[1],
+        'website': tr.find_all('td')[1].find_all('a')[1].get('href') if len(tr.find_all('td')[1].find_all('a')) > 1 else None,
+            }
+            for tr in profs_html
+        }
+    with open('out.json', 'w') as f:
+        f.write(json.dumps(profs_dict))
+
+    
+
+if __name__ == '__main__':
+    main()
